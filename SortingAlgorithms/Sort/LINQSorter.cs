ef31c1bcbf89d1162ddc6f98457fc3ea90d7ddb8
Juliano Ladeira <julianoladeira@gmail.com>
2019-10-01 00:11:36 -0300

    Added LINQSorter

---
+++ SortingAlgorithms/Sort/LINQSorter.cs
@@ -0,0 +1,21 @@
+﻿using SortingAlgorithms.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms.Sort
+{
+    public class LINQSorter<T> : Sorter<T> where T : IComparable
+    {
+        protected override List<T> DoSort(List<T> list)
+        {
+            List<T> sortedList = (
+                from value in list
+                orderby value ascending
+                select value).ToList();
+            return sortedList;
+        }
+    }
+}
---
+++ SortingAlgorithmsTests/Sort/LINQSorterTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortingAlgorithms.Sort;
+
+namespace SortingAlgorithmsTests.Sort
+{
+    [TestClass]
+    public class LINQSorterTest : GenericSorterTest
+    {
+        public LINQSorterTest() : base(new LINQSorter<int>())
+        {
+        }
+    }
+}
